6ed9df9f28fe6d8077fb4e78a054d450a9fa126c
AtsushiHosaka <hosaka2917@gmail.com>
2023-04-10 21:06:17 +0900

    修正

--- Timer/Timer/ViewController.swift
+++ Timer/Timer/ViewController.swift
@@ -5,7 +5,7 @@ class ViewController: UIViewController {
     var timer: Timer!
     
     //残りの秒数の変数
-    var remainTime: Int = 0
+    var countdown: Int = 0
 
     @IBOutlet weak var timerLabel: UILabel!
     
@@ -19,38 +19,33 @@ class ViewController: UIViewController {
     //30秒のボタンが押された時
     @IBAction func select30seconds(_ sender: Any) {
         
-        remainTime = 30
-        
-        startTimer()
+        startTimer(time: 30)
     }
     
     //1分のボタンが押された時
     @IBAction func select1minute(_ sender: Any) {
         
-        remainTime = 60
-        
-        startTimer()
+        startTimer(time: 60)
     }
     
     //5分のボタンが押された時
     @IBAction func select5minutes(_ sender: Any) {
         
         //5分=300秒
-        remainTime = 300
-        
-        startTimer()
+        startTimer(time: 300)
     }
     
     //10分のボタンが押された時
     @IBAction func select10minutes(_ sender: Any) {
         
-        remainTime = 600
-        
-        startTimer()
+        startTimer(time: 600)
     }
     
     //timerを動かす
-    func startTimer() {
+    //time引数で、タイマーの長さを設定する
+    func startTimer(time: Int) {
+        
+        countdown = time
         
         //もしすでにtimerが動いていたら二重でタイマーが動いてしまうので、元々のタイマーを終わらせる
         if timer != nil {
@@ -74,10 +69,10 @@ class ViewController: UIViewController {
         
         updateLabel()
         
-        remainTime -= 1
+        countdown -= 1
         
         //もし残り時間が0未満なら終わり
-        if remainTime < 0 {
+        if countdown < 0 {
             
             showStopAlert()
             stopTimer()
@@ -87,15 +82,15 @@ class ViewController: UIViewController {
     //残り秒数のlabelに表示される文章を変える
     func updateLabel() {
         
-        //残りの分数→remainTimeを60で割って切り捨て
-        //残りの秒数→remainTimeを60で割ったあまり
-        let remainMinutes: Int = remainTime / 60
-        let remainSeconds: Int = remainTime % 60
+        //残りの分数→countdownを60で割って切り捨て
+        //残りの秒数→countdownを60で割ったあまり
+        let remainingMinutes: Int = countdown / 60
+        let remainingSeconds: Int = countdown % 60
         
-        //%02dには、あとに書いてある変数（ここではremainMinutesとremainSeconds）がそれぞれ入ります。
+        //%02dには、あとに書いてある変数（ここではremainingMinutesとremainingSeconds）がそれぞれ入ります。
         //また、%02dという書き方をしているため、もし代入された値が1桁なら0で埋めて表示されるようになっています。
-        //例：remainMinutes=1、remainSeconds=5が代入されたら、01:05と表示
-        timerLabel.text = String(format: "%02d:%02d", remainMinutes, remainSeconds)
+        //例：remainingMinutes=1、remainingSeconds=5が代入されたら、01:05と表示
+        timerLabel.text = String(format: "%02d:%02d", remainingMinutes, remainingSeconds)
     }
 
     //残り時間が0になったことを知らせるアラートを表示
